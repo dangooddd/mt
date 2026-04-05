e6989c85e7bbce515e44674702fb3f6d0a6d240f
dangooddd <dangoodds@gmail.com>
2026-04-05 15:15:49 +0300

    Add validate script and luong attention lstm model

--- src/mt/dataset/validate.py
+++ src/mt/dataset/validate.py
@@ -1,23 +1,44 @@
 from argparse import ArgumentParser
+from pathlib import Path
 
 import transformers
+from datasets import load_from_disk
 from transformers import pipeline
 
 transformers.logging.set_verbosity_error()
 pipe = pipeline("text-classification", model="papluca/xlm-roberta-base-language-detection")
 
 
-def add_language_validation(batch, column: str, expected: str):
+def add_language_validation(batch, feature_name: str, column: str, expected: str):
     texts = batch[column]
-    pipe(texts, top_k=1, truncation=True)
+    results = pipe(texts, truncation=True)
+    valid = [x["label"] == expected for x in results]
+    return {feature_name: valid}
 
 
 def main():
     parser = ArgumentParser("Determine correctness of language in dataset")
-    parser.add_argument("--dataset", type=str)
+    parser.add_argument("--dataset-path", type=str)
+    parser.add_argument("--feature-name", type=str)
     parser.add_argument("--column", type=str)
     parser.add_argument("--expected", type=str)
     args = parser.parse_args()
+
+    dataset = load_from_disk(args.dataset_path)
+
+    scored = dataset.map(
+        add_language_validation,
+        fn_kwargs={
+            "feature_name": args.feature_name,
+            "column": args.column,
+            "expected": args.expected,
+        },
+        batched=True,
+        batch_size=512,
+        load_from_cache_file=False,
+    )
+
+    scored.save_to_disk(Path(args.dataset_path).with_suffix(f".validated-{args.column}"))
 
 
 if __name__ == "__main__":
