606c3bb1bf1cb4e0bbb30bd73a89a324b454a59d
dangooddd <dangoodds@gmail.com>
2026-04-08 01:32:37 +0000

    Move luong to script format

--- scripts/luong.py
+++ scripts/luong.py
@@ -19,124 +19,126 @@ from mt.models.train import (
 )
 from mt.tokenizers import UnigramTokenizer
 
-freeze_support()
 
-MAX_LR = 0.001
-MIN_LR = 0.000001
-BATCH_SIZE = 5000
-EPOCH_STEPS = 50000
-WARMUP_STEPS = 50000
-STEPS = 5000000
-MAX_LENGTH = 1024
-EXPERIMENT = "luong-v1"
+def main():
+    MAX_LR = 0.001
+    MIN_LR = 0.000001
+    BATCH_SIZE = 100
+    EPOCH_STEPS = 500
+    WARMUP_STEPS = 5000
+    STEPS = 50000
+    MAX_LENGTH = 192
+    EXPERIMENT = "luong-v1"
 
-dataset = load_from_disk("data/datasets/opus-100-final")
-tokenizer_ru = UnigramTokenizer.from_file("data/tokenizers/ru-unigram-24000.json")
-tokenizer_en = UnigramTokenizer.from_file("data/tokenizers/en-unigram-24000.json")
+    dataset = load_from_disk("data/datasets/opus-100-final")
+    tokenizer_ru = UnigramTokenizer.from_file("data/tokenizers/ru-unigram-24000.json")
+    tokenizer_en = UnigramTokenizer.from_file("data/tokenizers/en-unigram-24000.json")
 
-model = LuongSeq2Seq(
-    src_vocab_size=tokenizer_ru.get_vocab_size(),
-    tgt_vocab_size=tokenizer_en.get_vocab_size(),
-    src_pad_token_id=tokenizer_ru.pad_token_id,
-    tgt_pad_token_id=tokenizer_en.pad_token_id,
-    tgt_bos_token_id=tokenizer_en.bos_token_id,
-    tgt_eos_token_id=tokenizer_en.eos_token_id,
-    embedding_dim=1500,
-    hidden_dim=1500,
-    num_layers=6,
-)
+    model = LuongSeq2Seq(
+        src_vocab_size=tokenizer_ru.get_vocab_size(),
+        tgt_vocab_size=tokenizer_en.get_vocab_size(),
+        src_pad_token_id=tokenizer_ru.pad_token_id,
+        tgt_pad_token_id=tokenizer_en.pad_token_id,
+        tgt_bos_token_id=tokenizer_en.bos_token_id,
+        tgt_eos_token_id=tokenizer_en.eos_token_id,
+        embedding_dim=1024,
+        hidden_dim=1024,
+        num_layers=4,
+    )
 
-optimizer = optim.AdamW(model.parameters(), lr=MAX_LR)
-criterion = CrossEntropyLoss(ignore_index=tokenizer_en.pad_token_id, label_smoothing=0.1)
+    optimizer = optim.AdamW(model.parameters(), lr=MAX_LR)
+    criterion = CrossEntropyLoss(ignore_index=tokenizer_en.pad_token_id, label_smoothing=0.1)
 
-warmup_scheduler = optim.lr_scheduler.LinearLR(
-    optimizer,
-    start_factor=0.1,
-    end_factor=1.0,
-    total_iters=WARMUP_STEPS,
-)
+    warmup_scheduler = optim.lr_scheduler.LinearLR(
+        optimizer,
+        start_factor=0.1,
+        end_factor=1.0,
+        total_iters=WARMUP_STEPS,
+    )
 
-cosine_scheduler = optim.lr_scheduler.CosineAnnealingLR(
-    optimizer,
-    T_max=(STEPS - WARMUP_STEPS),
-    eta_min=MIN_LR,
-)
+    cosine_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+        optimizer,
+        T_max=(STEPS - WARMUP_STEPS),
+        eta_min=MIN_LR,
+    )
 
-scheduler = optim.lr_scheduler.SequentialLR(
-    optimizer,
-    schedulers=[warmup_scheduler, cosine_scheduler],
-    milestones=[WARMUP_STEPS],
-)
+    scheduler = optim.lr_scheduler.SequentialLR(
+        optimizer,
+        schedulers=[warmup_scheduler, cosine_scheduler],
+        milestones=[WARMUP_STEPS],
+    )
 
-collate_fn = build_collate_fn(
-    src_tokenizer=tokenizer_ru,
-    tgt_tokenizer=tokenizer_en,
-    max_src_length=1024,
-    max_tgt_length=1024,
-    src_column="ru",
-    tgt_column="en",
-)
+    collate_fn = build_collate_fn(
+        src_tokenizer=tokenizer_ru,
+        tgt_tokenizer=tokenizer_en,
+        max_src_length=MAX_LENGTH,
+        max_tgt_length=MAX_LENGTH,
+        src_column="ru",
+        tgt_column="en",
+    )
 
-train_loader = DataLoader(
-    cast(Dataset, dataset["train"]),
-    batch_size=BATCH_SIZE,
-    collate_fn=collate_fn,
-    num_workers=8,
-    shuffle=True,
-)
+    train_loader = DataLoader(
+        cast(Dataset, dataset["train"]),
+        batch_size=BATCH_SIZE,
+        collate_fn=collate_fn,
+        num_workers=0,
+        shuffle=True,
+    )
 
-evaluation_loader = DataLoader(
-    cast(Dataset, dataset["validation"]),
-    batch_size=BATCH_SIZE,
-    collate_fn=collate_fn,
-    num_workers=8,
-)
+    evaluation_loader = DataLoader(
+        cast(Dataset, dataset["validation"]),
+        batch_size=BATCH_SIZE,
+        collate_fn=collate_fn,
+        num_workers=0,
+    )
 
-trainer = create_trainer(
-    model=model,
-    optimizer=optimizer,
-    criterion=criterion,
-    scheduler=scheduler,
-    compute_loss=compute_loss,
-)
+    trainer = create_trainer(
+        model=model,
+        optimizer=optimizer,
+        criterion=criterion,
+        scheduler=scheduler,
+        compute_loss=compute_loss,
+    )
 
-evaluator = create_evaluator(
-    model=model,
-    criterion=criterion,
-    compute_loss=compute_loss,
-    compute_predictions=compute_predictions,
-    compute_predictions_kwargs={"tgt_tokenizer": tokenizer_en, "max_length": MAX_LENGTH},
-)
+    evaluator = create_evaluator(
+        model=model,
+        criterion=criterion,
+        compute_loss=compute_loss,
+        compute_predictions=compute_predictions,
+        compute_predictions_kwargs={"tgt_tokenizer": tokenizer_en, "max_length": MAX_LENGTH},
+    )
 
-train_tb_logger = attach_tensorboard_logging(
-    trainer,
-    f"data/runs/{EXPERIMENT}",
-    tag="train",
-    every=100,
-)
+    train_tb_logger = attach_tensorboard_logging(
+        trainer,
+        f"data/runs/{EXPERIMENT}",
+        tag="train",
+        every=100,
+    )
 
-evaluation_tb_logger = attach_tensorboard_logging(
-    evaluator,
-    f"data/runs/{EXPERIMENT}",
-    tag="evaluation",
-)
+    evaluation_tb_logger = attach_tensorboard_logging(
+        evaluator,
+        f"data/runs/{EXPERIMENT}",
+        tag="evaluation",
+    )
 
-ProgressBar().attach(trainer, metric_names=["loss"])
+    ProgressBar().attach(trainer, metric_names=["loss"])
+
+    @trainer.on(Events.COMPLETED)
+    def _close_loggers(_):
+        train_tb_logger.close()
+        evaluation_tb_logger.close()
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def run_validation(_):
+        evaluator.run(evaluation_loader)
+
+    trainer.run(
+        train_loader,
+        max_epochs=STEPS // EPOCH_STEPS,
+        epoch_length=EPOCH_STEPS,
+    )
 
 
-@trainer.on(Events.COMPLETED)
-def _close_loggers(_):
-    train_tb_logger.close()
-    evaluation_tb_logger.close()
-
-
-@trainer.on(Events.EPOCH_COMPLETED)
-def run_validation(_):
-    evaluator.run(evaluation_loader)
-
-
-trainer.run(
-    train_loader,
-    max_epochs=STEPS // EPOCH_STEPS,
-    epoch_length=EPOCH_STEPS,
-)
+if __name__ == "__main__":
+    freeze_support()
+    main()
